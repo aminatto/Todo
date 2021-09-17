f112eb365a21d8387b69236add3e7c2662262642
Danilo Shibata <danilo.shibata@kobold.com.br>
2021-09-17 11:20:58 -0300

    Implementa gerenciamento de tarefas.
    Implementa as funcionalidades para manipulação de tarefas.
    
    * Controller delega as tarefas ao serviço, separando a camada
    de apresentação da camada de aplicação.
    * Serviço implementa o armazenamento dos objetos criados em
    memória, utilizando uma lista para manter a coleção de `Todo`
    criados na execução da aplicação.

--- src/Kobold.TodoApp.Api/Controllers/TodosController.cs
+++ src/Kobold.TodoApp.Api/Controllers/TodosController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Kobold.TodoApp.Api.Models;
+using Kobold.TodoApp.Api.Services;
 
 namespace Kobold.TodoApp.Api.Controllers
 {
@@ -14,41 +15,43 @@ namespace Kobold.TodoApp.Api.Controllers
     public class TodosController : ControllerBase
     {
 
+        private readonly TodoService _todoService;
         private readonly ILogger<TodosController> _logger;
 
         public TodosController(ILogger<TodosController> logger)
         {
             _logger = logger;
+            _todoService = new TodoService();
         }
 
         [HttpGet]
         public IEnumerable<Todo> Get()
         {
-            throw new NotImplementedException();
+            return _todoService.Get();
         }
 
         [HttpPost]
         public Todo Create([FromBody] TodoViewModel todovm)
         {
-            throw new NotImplementedException();
+            return _todoService.Create(todovm);
         }
 
         [HttpPut("{id}")]
         public Todo Update([FromRoute] int id, [FromBody] TodoViewModel todovm)
         {
-            throw new NotImplementedException();
+            return _todoService.Update(id, todovm);
         }
 
         [HttpGet("{id}")]
         public Todo Get([FromRoute] int id)
         {
-            throw new NotImplementedException();
+            return _todoService.Get(id);
         }
 
         [HttpDelete("{id}")]
         public void Remove([FromRoute] int id)
         {
-            throw new NotImplementedException();
+            _todoService.Remove(id);
         }
 
     }
---
+++ src/Kobold.TodoApp.Api/Services/TodoService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Kobold.TodoApp.Api.Models;
+
+namespace Kobold.TodoApp.Api.Services
+{
+
+    public class TodoService
+    {
+
+        private static int nextId = 1;
+        private static List<Todo> Todos = new List<Todo>();
+
+        public IEnumerable<Todo> Get()
+        {
+            return Todos;
+        }
+
+        public Todo Create(TodoViewModel todovm)
+        {
+            var todo = new Todo { Id = nextId++, DataCriacao = DateTime.Now, Description = todovm.Description, Done = todovm.Done }; 
+            Todos.Add(todo);
+
+            return todo;
+        }
+
+        public Todo Update(int id, TodoViewModel todovm)
+        {
+            var todo = Todos.Single(todo => todo.Id == id);
+            todo.Description = todovm.Description;
+            todo.Done = todovm.Done;
+
+            return todo;
+        }
+
+        public Todo Get(int id)
+        {
+            return Todos.Single(todo => todo.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var index = Todos.FindIndex(todo => todo.Id == id);
+            Todos.RemoveAt(index);
+        }
+
+    }
+}
