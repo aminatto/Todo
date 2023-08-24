cc4728cf38f837a8c6ef29c6e14e77c50902bb3c
aminatto <minattoamanda@gmail.com>
2023-08-23 22:39:10 -0300

    Define configurações iniciais.
    Configuração de injeção de dependencias, conexão com banco, e estabelecendo relacionamentos das entidades na classe AppDbContext.
    Swagger adicionado com o intuito de facilitar o teste.

---
+++ src/Kobold.TodoApp.Api/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Kobold.TodoApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace Kobold.TodoApp.Api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        public DbSet<TodoCollection> TodoCollections { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>()
+                .HasOne(t => t.TodoCollection)
+                .WithMany(c => c.Todos)
+                .HasForeignKey(t => t.TodoCollectionId)
+                .IsRequired(false)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
--- src/Kobold.TodoApp.Api/Startup.cs
+++ src/Kobold.TodoApp.Api/Startup.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Kobold.TodoApp.Api.Context;
+using Kobold.TodoApp.Api.Controllers;
+using Kobold.TodoApp.Api.Models;
+using Kobold.TodoApp.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Kobold.TodoApp.Api
 {
@@ -26,6 +33,16 @@ namespace Kobold.TodoApp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api TODO Kobold", Version = "v1" });
+            });
+
+            services.AddScoped<ITodoCollectionService, TodoCollectionService>();
+            services.AddScoped<ITodoService, TodoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +51,12 @@ namespace Kobold.TodoApp.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api TODO Kobold");
+                });
+
             }
 
             app.UseHttpsRedirection();
